5f487ac0d8a32077795bc5a346d5722c17ad20e4
ColeMinersUnion <colehansen018@gmail.com>
2024-07-06 14:14:40 -0400

    Making all of the pages for the React-Router to nav between pages.

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import App from './App.jsx'
+import Career from './pages/Career.jsx'
+import JobPrep from './pages/JobPrep.jsx'
+import Profile from './pages/Profile.jsx'
+import SignUp from './pages/SignUp.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App />
+  },
+  {
+    path:'profile',
+    element: <Profile/>
+  },
+  {
+    path:'career',
+    element:<Career/>
+  },
+  {
+    path:'JobPrep',
+    element:<JobPrep/>
+  },
+  {
+    path:'Signup',
+    element:<SignUp/>
+  }
+])
+
+//Initializing the webpage
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
