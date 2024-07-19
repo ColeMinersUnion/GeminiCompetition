f76118e14350861a5038290a52ef075fe1843099
ColeMinersUnion <colehansen018@gmail.com>
2024-07-19 13:52:46 -0400

    Changed some config stuff.

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     './index.html',
-    './src/**/*.{jsx}'
+    './src/**/*.jsx'
   ],
   theme: {
     extend: {},
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 8000,
     proxy:{
       '/api':{
-        target:'http://127.0.0.1:8080',
+        target:'http://localhost:8080',
         changeOrigin: true,
         secure: false,
       }
