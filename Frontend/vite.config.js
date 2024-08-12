98cee537bd48095182bb0639ce5273038da025ec
ColeMinersUnion <colehansen018@gmail.com>
2024-08-12 16:47:36 -0400

    It's not on a port it's just a url now

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
-    host:true,
-    strictPort:true,
-    port: 8000,
     proxy:{
       '/api':{
         target:'https://sprout-backend-khssbqm7ma-uk.a.run.app',
