179affbd1e26be5dc993df4575052d81c8b6d56f
ColeMinersUnion <colehansen018@gmail.com>
2024-08-12 16:31:43 -0400

    Maybe netlify will see it this time?

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 8000,
     proxy:{
       '/api':{
-        target:'http://localhost:8080',
+        target:'https://sprout-backend-khssbqm7ma-uk.a.run.app',
         changeOrigin: true,
         secure: false,
       }
