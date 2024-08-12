6aefd6c26c587ca60887c0f59ce8404d6e5fce21
ColeMinersUnion <colehansen018@gmail.com>
2024-08-12 10:22:51 -0400

    Changed up the footer. Most of the links changed

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import '../pages/css/Footer.css';
-
+import { Link } from 'react-router-dom';
 export default function Footer() {
     return (
         <>
@@ -18,46 +18,42 @@ export default function Footer() {
 
                     <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:col-span-2 lg:grid-cols-4">
                         <div>
-                            <p className="section-title">Services</p>
+                            <p className="section-title">Features</p>
                             <ul className="mt-6 space-y-4 text-sm">
-                                <li><a href="#" className="text-white transition hover:opacity-75">1on1 Coaching</a></li>
-                                <li><a href="#" className="text-white transition hover:opacity-75">Company Review</a></li>
-                                <li><a href="#" className="text-white transition hover:opacity-75">Accounts Review</a></li>
-                                <li><a href="#" className="text-white transition hover:opacity-75">HR Consulting</a></li>
-                                <li><a href="#" className="text-white transition hover:opacity-75">SEO Optimisation</a></li>
+                                <li><Link to="/Career" className="text-white transition hover:opacity-75">Career Discussion and Exploration</Link></li>
+                                <li><Link to="/Job" className="text-white transition hover:opacity-75">Job Posting Review</Link></li>
+                                <li><Link to="/Job" className="text-white transition hover:opacity-75">Resume Review and Job Matching</Link></li>
                             </ul>
                         </div>
 
                         <div>
                             <p className="section-title">Company</p>
                             <ul className="mt-6 space-y-4 text-sm">
-                                <li><a href="#" className="text-white transition hover:opacity-75">About</a></li>
-                                <li><a href="#" className="text-white transition hover:opacity-75">Meet the Team</a></li>
-                                <li><a href="#" className="text-white transition hover:opacity-75">Accounts Review</a></li>
+                                <li><Link to="/" className="text-white transition hover:opacity-75">About</Link></li>
+                                <li><Link to="/Contact" className="text-white transition hover:opacity-75">Meet the Team</Link></li>
                             </ul>
                         </div>
 
                         <div>
-                            <p className="section-title">Helpful Links</p>
+                            <p className="section-title">Tech Stack</p>
                             <ul className="mt-6 space-y-4 text-sm">
-                                <li><a href="#" className="text-white transition hover:opacity-75">Contact</a></li>
-                                <li><a href="#" className="text-white transition hover:opacity-75">FAQs</a></li>
-                                <li><a href="#" className="text-white transition hover:opacity-75">Live Chat</a></li>
+                                <li><a href="https://flask.palletsprojects.com/en/3.0.x/" className="text-white transition hover:opacity-75">Backend: Flask</a></li>
+                                <li><a href="https://vitejs.dev/" className="text-white transition hover:opacity-75">Frontend: Vite</a></li>
+                                <li><a href="https://react.dev/" className="text-white transition hover:opacity-75">Frontend: React</a></li>
                             </ul>
                         </div>
 
                         <div>
                             <p className="section-title">Legal</p>
                             <ul className="mt-6 space-y-4 text-sm">
-                                <li><a href="#" className="text-white transition hover:opacity-75">Accessibility</a></li>
-                                <li><a href="#" className="text-white transition hover:opacity-75">Returns Policy</a></li>
-                                <li><a href="#" className="text-white transition hover:opacity-75">Refund Policy</a></li>
-                                <li><a href="#" className="text-white transition hover:opacity-75">Hiring Statistics</a></li>
+                                <li><a href="https://github.com/ColeMinersUnion/GeminiCompetition/blob/main/LICENSE" className="text-white transition hover:opacity-75">License</a></li>
+                                <li><a href="https://ai.google.dev/gemini-api/terms" className="text-white transition hover:opacity-75">Google Gemini Policy</a></li>
+                                <li><a href="https://ai.google.dev/competition" className="text-white transition hover:opacity-75">Terms of the Competition</a></li>
                             </ul>
                         </div>
                     </div>
                 </div>
-                <p className="footer-copy text-xs text-gray-500">&copy; 2022. Company Name. All rights reserved.</p>
+                <p className="footer-copy text-xs text-gray-500">&copy; 2024. Sprout. All rights reserved.</p>
             </footer>
         </>
     );
